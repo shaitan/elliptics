87800f414411b2c9b49a323ad361b8972fc98108
Ivan Chelubeev <ivan.chelubeev@gmail.com>
2016-04-27 21:10:31 +0300

    fix return value of dnet_update_backend_weight

--- library/node.c
+++ library/node.c
@@ -2,17 +2,17 @@
  * Copyright 2008+ Evgeniy Polyakov <zbr@ioremap.net>
  *
  * This file is part of Elliptics.
- * 
+ *
  * Elliptics is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Elliptics is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Elliptics.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -648,12 +648,12 @@ void dnet_set_backend_weight(struct dnet_net_state *st, int backend_id, uint32_t
 	pthread_rwlock_unlock(&st->idc_lock);
 }
 
-void dnet_update_backend_weight(struct dnet_net_state *st, const struct dnet_cmd *cmd, uint64_t ioflags, long time) {
+int dnet_update_backend_weight(struct dnet_net_state *st, const struct dnet_cmd *cmd, uint64_t ioflags, long time) {
 	double old_weight = 0., new_weight = 0.;
 	if (!st)
-		return;
+		return -ENOENT;
 
-	const err = dnet_get_backend_weight(st, cmd->backend_id, ioflags, &old_weight);
+	const int err = dnet_get_backend_weight(st, cmd->backend_id, ioflags, &old_weight);
 	if (!err &&
 	    cmd->status == 0 &&
 	    cmd->size) {
