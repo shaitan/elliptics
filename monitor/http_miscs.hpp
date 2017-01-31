1ca75d2e44c8957daf6fbd2bf499c4f72e8f40f6
Kirill Smorodinnikov <shaitkir@gmail.com>
2017-01-31 11:30:50 +0300

    monitor: refactor http server and add final log
    Final log is printed for all successful requests and contains several timings.

--- monitor/http_miscs.hpp
+++ monitor/http_miscs.hpp
@@ -22,108 +22,48 @@
 
 #include <string>
 
-#include <boost/lexical_cast.hpp>
-
 namespace ioremap { namespace monitor {
 
 namespace status_strings {
-const std::string not_found = "HTTP/1.1 404 Not Found\r\n";
-const std::string bad_request = "HTTP/1.1 400 Bad Request\r\n";
 const std::string ok = "HTTP/1.1 200 OK\r\n";
 }
 
 namespace content_strings {
-const std::string not_found = "<html>\n"
-	"<head><title>Not Found</title></head>\n"
-	"<body><h1>404 Not Found</h1></body>\n"
-	"</html>\n";
-const std::string bad_request = "<html>\n"
-	"<head><title>Bad Request</title></head>\n"
-	"<body><h1>400 Bad Request</h1></body>\n"
-	"</html>\n";
-const std::string list = "<html>\n"
-	"<body>\n"
-	"GET <a href='/list'>/list</a> - Retrieves a list of acceptable statistics<br/>\n"
-	"GET <a href='/all'>/all</a> - Retrieves all statistics from all submodules<br/>\n"
-	"GET <a href='/cache'>/cache</a> - Retrieves statistics about cache<br/>\n"
-	"GET <a href='/io'>/io</a> - Retrieves statistics about io statistics<br/>\n"
-	"GET <a href='/commands'>/commands</a> - Retrieves statistics about commands<br/>\n"
-	"GET <a href='/io_histograms'>/io_histograms</a> - Retrieves statistics about io histograms<br/>\n"
-	"GET <a href='/backend'>/backend</a> - Retrieves statistics about backend<br/>\n"
-	"GET <a href='/stats'>/stats</a> - Retrieves in-process runtime statistics<br/>\n"
-	"GET <a href='/procfs'>/procfs</a> - Retrieves system statistics about process<br/>\n"
-	"GET <a href='/top'>/top</a> - Retrieves statistics of top keys ordered by generated traffic<br/>\n"
-	"</body>\n"
-	"</html>\n";
+const std::string list = R"(<html>
+	<body>
+		GET <a href='/list'>/list</a> - Retrieves a list of acceptable statistics<br/>
+		GET <a href='/all'>/all</a> - Retrieves all statistics from all submodules<br/>
+		GET <a href='/cache'>/cache</a> - Retrieves statistics about cache<br/>
+		GET <a href='/io'>/io</a> - Retrieves statistics about io statistics<br/>
+		GET <a href='/commands'>/commands</a> - Retrieves statistics about commands<br/>
+		GET <a href='/io_histograms'>/io_histograms</a> - Retrieves statistics about io histograms<br/>
+		GET <a href='/backend'>/backend</a> - Retrieves statistics about backend<br/>
+		GET <a href='/stats'>/stats</a> - Retrieves in-process runtime statistics<br/>
+		GET <a href='/procfs'>/procfs</a> - Retrieves system statistics about process<br/>
+		GET <a href='/top'>/top</a> - Retrieves statistics of top keys ordered by generated traffic<br/>
+	</body>
+</html>)";
 }
-
-const std::string categories_url = "/?categories=";
-
-const std::map<std::string, uint64_t> handlers = {
-	{"/all", DNET_MONITOR_ALL},
-	{"/cache", DNET_MONITOR_CACHE},
-	{"/io", DNET_MONITOR_IO},
-	{"/commands", DNET_MONITOR_COMMANDS},
-	{"/backend", DNET_MONITOR_BACKEND},
-	{"/stats", DNET_MONITOR_STATS},
-	{"/procfs", DNET_MONITOR_PROCFS},
-	{"/top", DNET_MONITOR_TOP}
-};
 
 /*!
  * Generates HTTP response for @req category with @content
  */
 std::string make_reply(uint64_t req, std::string content = "") {
-	std::stringstream ret;
+	std::ostringstream ret;
 	std::string content_type = "application/json";
 	if (req == 0) {
-		ret << status_strings::ok;
 		content = content_strings::list;
 		content_type = "text/html";
-	} else {
-		ret << status_strings::ok;
 	}
 
-	ret << "Content-Type: " << content_type << "\r\n"
-		<< "Content-Length: " << std::to_string((long long unsigned int)content.size()) << "\r\n"
-		<< (req != 0 ? "Content-Encoding: deflate\r\n" : "")
-		<< "Connection: close\r\n"
-		<< "\r\n"
-		<< content;
+	ret << status_strings::ok
+	    << "Content-Type: " << content_type << "\r\n"
+	    << (req != 0 ? "Content-Encoding: deflate\r\n" : "")
+	    << "Connection: close\r\n"
+	    << "Content-Length: " << content.size() << "\r\n\r\n"
+	    << content;
 
 	return ret.str();
-}
-
-/*!
- * Parses simple HTTP request and determines requested category
- * @packet - HTTP request packet
- * @size - size of HTTP request packet
- */
-uint64_t parse(const char* packet, size_t size) {
-	const char* end = packet + size;
-	const char *method_end = std::find(packet, end, ' ');
-	if (method_end >= end || packet == method_end)
-		return 0;
-
-	const char *url_begin = method_end + 1;
-	const char *url_end = std::find(url_begin, end, ' ');
-	if (url_end >= end)
-		return 0;
-
-	auto it = handlers.find(std::string(url_begin, url_end));
-	if (it != handlers.end())
-		return it->second;
-	else if (ssize_t(categories_url.size()) < (url_end - url_begin) &&
-	         strncmp(url_begin, categories_url.c_str(), categories_url.size()) == 0) {
-		const char *categories = url_begin + categories_url.size();
-		try {
-			return boost::lexical_cast<uint64_t>(std::string(categories, url_end));
-		} catch(...) {
-			printf("Couldn't parse categories: %s\n", categories);
-		}
-	}
-
-	return 0;
 }
 
 }} /* namespace ioremap::monitor */
--- monitor/server.cpp
+++ monitor/server.cpp
@@ -20,55 +20,79 @@
 #include "server.hpp"
 #include "monitor.hpp"
 
+#include <chrono>
+
 #include "library/elliptics.h"
 #include "library/logger.hpp"
 #include "http_miscs.hpp"
 
 namespace ioremap { namespace monitor {
+std::string to_string(const boost::asio::ip::tcp::endpoint &endpoint, bool use_bracket=false) {
+	std::ostringstream stream;
+	const auto &address = endpoint.address();
+	if (address.is_v6() && address.to_v6().is_v4_mapped()) {
+		stream << address.to_v6().to_v4() << ":" << endpoint.port();
+	} else if (use_bracket) {
+		stream << endpoint;
+	} else {
+		stream << address << ":" << endpoint.port();
+	}
+	return stream.str();
+}
 
-class handler: public std::enable_shared_from_this<handler> {
+class handler : public std::enable_shared_from_this<handler> {
 public:
-	handler(monitor &mon, boost::asio::io_service &io_service)
+	handler(monitor &mon, boost::asio::ip::tcp::socket socket)
 	: m_monitor(mon)
-	, m_socket(io_service)
-	, m_remote("")
-	{}
+	, m_socket(std::move(socket))
+	, m_remote(to_string(m_socket.remote_endpoint())) {}
 
 	void start() {
-		m_remote = m_socket.remote_endpoint().address().to_string();
-		DNET_LOG_INFO(m_monitor.node(), "monitor: server: accepted client: {}:{}", m_remote,
-		              m_socket.remote_endpoint().port());
+		DNET_LOG_INFO(m_monitor.node(), "monitor: http-server: accepted client: {}", m_remote);
 		async_read();
-	}
-
-	boost::asio::ip::tcp::socket &socket() {
-		return m_socket;
 	}
 
 private:
 	void async_read();
-	void async_write(std::string data);
-	void handle_read(const boost::system::error_code &err, size_t size);
-	void handle_write();
+	void async_write();
 	void close();
 
 	uint64_t parse_request(size_t size);
 
-	monitor							&m_monitor;
-	boost::asio::ip::tcp::socket	m_socket;
-	std::string						m_remote;
-	boost::array<char, 1024>		m_buffer;
-	std::string						m_report;
+	monitor &m_monitor;
+	boost::asio::ip::tcp::socket m_socket;
+
+	const std::string m_remote;
+	std::array<char, 1024> m_buffer;
+	std::string m_response;
+	std::string m_url;
+
+	std::chrono::time_point<std::chrono::system_clock> m_start_ts;
+	std::chrono::time_point<std::chrono::system_clock> m_recv_ts;
+	std::chrono::time_point<std::chrono::system_clock> m_collect_ts;
 };
 
-static boost::asio::ip::tcp convert_family(int family) {
+inline static boost::asio::ip::tcp convert_family(int family) {
 	return family == AF_INET6 ? boost::asio::ip::tcp::v6() : boost::asio::ip::tcp::v4();
 }
 
-server::server(monitor &mon, unsigned int port, int family)
+server::server(monitor &mon, unsigned short port, int family)
 : m_monitor(mon)
-, m_acceptor(m_io_service, boost::asio::ip::tcp::endpoint(convert_family(family), port)) {
-	m_listen = std::thread(std::bind(&server::listen, this));
+, m_acceptor(m_io_service, {convert_family(family), port}) {
+	m_listen = std::thread([this]() {
+		try {
+			async_accept();
+			m_io_service.run();
+		} catch (const std::exception &e) {
+			DNET_LOG_ERROR(m_monitor.node(),
+			               "monitor: http-server: got exception: {}, exiting", e.what());
+		} catch (...) {
+			DNET_LOG_ERROR(m_monitor.node(),
+			               "monitor: http-server: got unknown exception, exiting");
+		}
+
+		dnet_set_need_exit(m_monitor.node());
+	});
 }
 
 server::~server() {
@@ -76,90 +100,149 @@ server::~server() {
 	m_listen.join();
 }
 
-void server::listen() {
-	while (!m_monitor.node()->need_exit) {
-		try {
-			async_accept();
-			m_io_service.run();
-		} catch (const std::exception &e) {
-			DNET_LOG_ERROR(m_monitor.node(), "monitor: server: got exception: {}, restarting it", e.what());
-		} catch (...) {
-			DNET_LOG_ERROR(m_monitor.node(), "monitor: server: got unknown exception, restarting");
-		}
-	}
-}
-
 void server::async_accept() {
-	auto h = std::make_shared<handler>(m_monitor, m_io_service);
-	m_acceptor.async_accept(h->socket(),
-	                        std::bind(&server::handle_accept, this,
-	                                  h,
-	                                  std::placeholders::_1));
+	auto socket = std::make_shared<boost::asio::ip::tcp::socket>(m_io_service);
+	m_acceptor.async_accept(*socket, [socket, this](const boost::system::error_code &err) {
+		if (!err) {
+			std::make_shared<handler>(m_monitor, std::move(*socket))->start();
+		}
+
+		async_accept();
+	});
 }
-
-void server::handle_accept(std::shared_ptr<handler> h, const boost::system::error_code &err) {
-	if (!err) {
-		h->start();
-	}
-
-	async_accept();
-}
-
 
 void server::stop() {
+	m_acceptor.close();
 	m_io_service.stop();
 }
 
 void handler::async_read() {
 	auto self(shared_from_this());
-	m_socket.async_read_some(boost::asio::buffer(m_buffer),
-	                         std::bind(&handler::handle_read, self,
-	                                   std::placeholders::_1,
-	                                   std::placeholders::_2));
+
+	m_start_ts = std::chrono::system_clock::now();
+	m_socket.async_read_some(boost::asio::buffer(m_buffer), [self, this](const boost::system::error_code &err,
+	                                                                     size_t size) {
+		if (err) {
+			DNET_LOG_ERROR(m_monitor.node(), "monitor: http-server: failed to receive request: {}",
+			               err.message());
+			close();
+			return;
+		}
+
+		m_recv_ts = std::chrono::system_clock::now();
+
+		const auto req = parse_request(size);
+
+		m_response = make_reply(req, [&]() {
+			if (req == 0)
+				return std::string();
+
+			DNET_LOG_DEBUG(m_monitor.node(),
+			               "monitor: http-server: got statistics request for categories: {:x} from: {}",
+			               req, m_remote);
+			return m_monitor.get_statistics().report(req);
+		}());
+
+		m_collect_ts = std::chrono::system_clock::now();
+
+		async_write();
+
+	});
 }
 
-void handler::handle_read(const boost::system::error_code &err, size_t size) {
-	if (err) {
-		close();
-		return;
-	}
+void handler::async_write() {
+	DNET_LOG_DEBUG(m_monitor.node(), "monitor: http-server: send requested statistics: started: {}, size: {}",
+	               m_remote, m_response.size());
 
-	auto req = parse_request(size);
-	std::string content = "";
-
-	if (req > 0) {
-		DNET_LOG_DEBUG(m_monitor.node(),
-		               "monitor: server: got statistics request for categories: {:x} from: {}:{}", req,
-		               m_remote, m_socket.remote_endpoint().port());
-		content = m_monitor.get_statistics().report(req);
-	}
-
-	std::string reply = make_reply(req, content);
-	async_write(reply);
-}
-
-void handler::async_write(std::string data) {
 	auto self(shared_from_this());
-	m_report = std::move(data);
-	DNET_LOG_DEBUG(m_monitor.node(), "monitor: server: send requested statistics: started: {}:{}, size: {}",
-	               m_remote, m_socket.remote_endpoint().port(), m_report.size());
-	boost::asio::async_write(m_socket, boost::asio::buffer(m_report),
-	                         std::bind(&handler::handle_write, self));
-}
 
-void handler::handle_write() {
-	DNET_LOG_DEBUG(m_monitor.node(), "monitor: server: send requested statistics: finished: {}:{}", m_remote,
-	               m_socket.remote_endpoint().port());
-	close();
+	boost::asio::async_write(m_socket, boost::asio::buffer(m_response), [self,
+	                                                                     this](const boost::system::error_code &err,
+	                                                                           const long unsigned int &size) {
+		const auto finish_ts = std::chrono::system_clock::now();
+
+		if (err) {
+			DNET_LOG_ERROR(m_monitor.node(), "monitor: http-server: failed to send response: {}",
+			               err.message());
+			return;
+		}
+
+		DNET_LOG_DEBUG(m_monitor.node(), "monitor: http-server: send requested statistics: finished: {}",
+		               m_remote);
+
+		const auto full_url = [this]() {
+			const auto endpoint = m_socket.local_endpoint();
+			const auto address = endpoint.address();
+
+			std::ostringstream stream;
+			stream << "http://" << to_string(m_socket.local_endpoint(), true) << m_url;
+			return stream.str();
+		}();
+
+		DNET_LOG_INFO(m_monitor.node(), "monitor: http-server: client: {}, url: \"{}\", response-size: {}, "
+		                                "recv-time: {} usecs, collect-time: {} usecs, send-time: {} usecs, "
+		                                "total-time: {} usecs",
+		              m_remote, full_url, size,
+		              std::chrono::duration_cast<std::chrono::microseconds>(m_recv_ts - m_start_ts).count(),
+		              std::chrono::duration_cast<std::chrono::microseconds>(m_collect_ts - m_recv_ts).count(),
+		              std::chrono::duration_cast<std::chrono::microseconds>(finish_ts - m_collect_ts).count(),
+		              std::chrono::duration_cast<std::chrono::microseconds>(finish_ts - m_start_ts).count());
+
+		close();
+	});
 }
 
 void handler::close() {
 	boost::system::error_code ec;
 	m_socket.shutdown(boost::asio::socket_base::shutdown_both, ec);
+	m_socket.close(ec);
 }
 
+/*!
+ * Parses simple HTTP request and determines requested category
+ * @packet - HTTP request packet
+ * @size - size of HTTP request packet
+ */
 uint64_t handler::parse_request(size_t size) {
-	return parse(m_buffer.data(), size);
+	static const std::string categories_url = "/?categories=";
+	static const std::map<std::string, uint64_t> handlers = {
+		{"/all", DNET_MONITOR_ALL},
+		{"/cache", DNET_MONITOR_CACHE},
+		{"/io", DNET_MONITOR_IO},
+		{"/commands", DNET_MONITOR_COMMANDS},
+		{"/backend", DNET_MONITOR_BACKEND},
+		{"/stats", DNET_MONITOR_STATS},
+		{"/procfs", DNET_MONITOR_PROCFS},
+		{"/top", DNET_MONITOR_TOP}
+	};
+
+	const char *packet = m_buffer.data();
+	const char* end = packet + size;
+	const char *method_end = std::find(packet, end, ' ');
+	if (method_end >= end || packet == method_end)
+		return 0;
+
+	const char *url_begin = method_end + 1;
+	const char *url_end = std::find(url_begin, end, ' ');
+	if (url_end >= end)
+		return 0;
+
+	m_url = std::string(url_begin, url_end);
+
+	auto it = handlers.find(m_url);
+	if (it != handlers.end()) {
+		return it->second;
+	} else if (ssize_t(categories_url.size()) < (url_end - url_begin) &&
+	           strncmp(url_begin, categories_url.c_str(), categories_url.size()) == 0) {
+		const char *categories = url_begin + categories_url.size();
+		try {
+			return std::stoull(std::string(categories, url_end));
+		} catch(...) {
+			DNET_LOG_ERROR(m_monitor.node(), "Can't parse categories: {}", categories);
+		}
+	}
+
+	return 0;
 }
 
 }} /* namespace ioremap::monitor */
