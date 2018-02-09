7690921137724755238a361b07cdd9b6bd71b83a
Kirill Smorodinnikov <shaitkir@gmail.com>
2018-02-09 11:02:45 +0300

    recovery: monir style fix

--- recovery/elliptics_recovery/recovery.py
+++ recovery/elliptics_recovery/recovery.py
@@ -340,8 +340,8 @@ def main(options, args):
         ctx.pool.close()
         ctx.pool.terminate()
         ctx.pool.join()
-    except Exception as e:
-        log.error("Recovering failed: %s, traceback: %s", repr(e), traceback.format_exc())
+    except Exception:
+        log.error("Recovering failed")
         ctx.pool.terminate()
         ctx.pool.join()
         result = False
@@ -424,11 +424,14 @@ def run(args=None):
                       help='Timeout for uncommitted records (prepared, but not committed).'
                       'Records that exceeded this timeout will be removed. [default: %default]')
     parser.add_option('-T', '--trace-id', action='store', dest="trace_id", default='0',
-                      help='Marks all recovery commands by trace_id at both recovery and server logs. This option accepts hex strings. [default: %default]')
+                      help=('Marks all recovery commands by trace_id at both recovery and server logs. '
+                            'This option accepts hex strings. [default: %default]'))
     parser.add_option('-U', '--no-server-send', action="store_true", dest="no_server_send", default=False,
-                      help='Do not use server-send for recovery. Disabling recovery via server-send useful if there is no network connection between groups')
+                      help=('Do not use server-send for recovery. Disabling recovery via server-send useful '
+                            'if there is no network connection between groups'))
     parser.add_option('--user-flags', action='append', dest='user_flags_set', default=[],
                       help='Recover key if at least one replica has user_flags from specified user_flags_set')
     parser.add_option('--data-flow-rate', action='store', dest='data_flow_rate', default=10,
-                      help='Expected execution speed for an I/O operation: server-send/read/write/etc. [default: %default] Mb')
+                      help=('Expected execution speed for an I/O operation: server-send/read/write/etc. '
+                            '[default: %default] Mb'))
     return main(*parser.parse_args(args))
