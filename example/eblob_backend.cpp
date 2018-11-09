1a4533074d825dd1a9013c3303312f1ca08ad98f
Artem Selishchev <arselishev@gmail.com>
2018-11-09 13:47:33 +0300

    backend: return -EILSEQ on lookup, read and server_send if record is corrupted

--- example/eblob_backend.cpp
+++ example/eblob_backend.cpp
@@ -172,6 +172,16 @@ int dnet_read_json_header(int fd, uint64_t offset, uint64_t size, dnet_json_head
 	return 0;
 }
 
+static int blob_read_and_check_flags_new(struct eblob_backend *b, struct eblob_key *key,
+                                         struct eblob_write_control *wc) {
+	int err = eblob_read_return(b, key, EBLOB_READ_NOCSUM, wc);
+	if (err == 0 && wc->flags & BLOB_DISK_CTL_CORRUPTED)
+		err = -EILSEQ;
+	if (err == 0 && wc->flags & BLOB_DISK_CTL_UNCOMMITTED)
+		err = -ENOENT;
+	return err;
+}
+
 int blob_file_info_new(eblob_backend_config *c, void *state, dnet_cmd *cmd, struct dnet_access_context *context) {
 	using namespace ioremap::elliptics;
 	eblob_backend *b = c->eblob;
@@ -186,10 +196,7 @@ int blob_file_info_new(eblob_backend_config *c, void *state, dnet_cmd *cmd, stru
 	memcpy(key.id, cmd->id.id, EBLOB_ID_SIZE);
 
 	eblob_write_control wc;
-	int err = eblob_read_return(b, &key, EBLOB_READ_NOCSUM, &wc);
-	if (err == 0 && wc.flags & BLOB_DISK_CTL_UNCOMMITTED)
-		err = -ENOENT;
-
+	int err = blob_read_and_check_flags_new(b, &key, &wc);
 	if (err) {
 		DNET_LOG_ERROR(c->blog, "{}: EBLOB: blob-file-info-new: failed: {} [{}]", dnet_dump_id(&cmd->id),
 		               strerror(-err), err);
@@ -397,10 +404,7 @@ static int blob_read_new_impl(eblob_backend_config *c,
 	memcpy(key.id, cmd->id.id, EBLOB_ID_SIZE);
 
 	eblob_write_control wc;
-	int err = eblob_read_return(b, &key, EBLOB_READ_NOCSUM, &wc);
-	if (err == 0 && wc.flags & BLOB_DISK_CTL_UNCOMMITTED)
-		err = -ENOENT;
-
+	int err = blob_read_and_check_flags_new(b, &key, &wc);
 	if (err) {
 		DNET_LOG_ERROR(c->blog, "{}: EBLOB: blob-read-new: failed: {} [{}]", dnet_dump_id(&cmd->id),
 		               strerror(-err), err);
@@ -1922,11 +1926,7 @@ int blob_send_new(struct eblob_backend_config *c,
 
 		memcpy(ekey.id, key.id, EBLOB_ID_SIZE);
 
-		err = eblob_read_return(c->eblob, &ekey, EBLOB_READ_NOCSUM, &wc);
-		if (err == 0 && wc.flags & BLOB_DISK_CTL_UNCOMMITTED) {
-			err = -ENOENT;
-		}
-
+		err = blob_read_and_check_flags_new(c->eblob, &ekey, &wc);
 		if (err) {
 			DNET_LOG_ERROR(c->blog, "{}: EBLOB: blob_send_new: lookup failed: {}", dnet_dump_id_str(key.id),
 			               dnet_print_error(err));
